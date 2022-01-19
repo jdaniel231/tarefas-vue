5b797115d84d9c038666344c6c1070f934227be4
hammer200911 <jdaniel231@gmail.com>
2022-01-19 12:04:20 -0300

    novo projeto tarefas

---
+++ src/views/Form.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mt-2">
+    <b-form>
+      <b-form-group 
+        label="Titulo" 
+        label-for="subject" 
+      >    
+        <b-form-input
+          id="subject"
+          v-model="form.subject"
+          type="text"
+          placeholder="Ex.: Lavar o carro"
+          autocomplete="off"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        label="Descricao" 
+        label-for="descriptiom" 
+      >    
+        <b-form-textarea
+          id="description"
+          v-model="form.description"
+          type="text"
+          placeholder="Ex.: Lavar o carro"
+          autocomplete="off"
+        ></b-form-textarea>
+      </b-form-group>
+
+      <b-button type="button" variant="outline-primary" >Salvar</b-button>
+
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+
+  data() {
+    return {
+      form: {
+        subject:"",
+        description: ""
+      }
+    }
+  }
+}
+</script>
